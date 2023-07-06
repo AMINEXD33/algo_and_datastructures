51e7ee96080790cfe2e839f1c02b2cbd0a391763
Amine Meftah <amine.gamer222@gmail.com>
2023-07-06 20:52:57 +0100

    update

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -3,6 +3,8 @@ class singly_linked_list:
 		self.type = 'S'
 		self.next = None
 		self.value = value
+		self.length = 0
+		self.last_ = None
 
 class doubly_linked_list:
 	def __init__(self, value):
@@ -10,6 +12,7 @@ class doubly_linked_list:
 		self.next = None
 		self.prev = None
 		self.value = value
+		self.length = 0 
 		### only for the last node 
 		self.last_ = None
 		
@@ -29,7 +32,9 @@ class handle_linked_list:
 			if current :current.next = node
 			else:current = node
 			current = node
+			root.length +=1
 		print('DONE !')
+		root.last_ = current
 		return root
 	def extract_singly_linked_list(root):
 		current_node = root
@@ -57,6 +62,7 @@ class handle_linked_list:
 
 			prev = current
 			current = node
+			root.length+=1
 		root.last_ = current
 		print('DONE !')
 		return root
@@ -78,9 +84,44 @@ class handle_linked_list:
 
 				print(current_node.value)
 				current_node = current_node.prev
-			
+	######
+	def insert_into_single_linked_list(root,value, mode):#(0 insert at the beginning)(1 insert at last)
+		node = singly_linked_list(value)
+		if mode == 0 :
+			node.next = root
+			node.last_ = root.last_
+			return node
+		elif mode == 1 :
+			root.last_.next = node
+			root.last_ = node
+			return root
+	def pop_first(root):
+		root_ = None
+		if root.next:
+			root_ = root.next
+			root_.last_ = root.last_
+			root.next = None
+		else:
+			print("No items left ")
+			return 0
+		return root_
 
-#root = handle_linked_list.create_single_linked_list([1,'amine','ezra',15,15241,'JS SUCKS(probably bc i suck at it :)'])
-#handle_linked_list.extract_singly_linked_list(root)
-root = handle_linked_list.create_doubly_linked_list([1,'amine','ezra',15,15241,'JS SUCKS(probably bc i suck at it :)'])
-handle_linked_list.extract_doubly_linked_list(root,1)
+	def pop_last(root):pass
+
+
+"""
+root = handle_linked_list.create_single_linked_list([1,'amine','ezra',15,15241,'JS SUCKS(probably bc i suck at it :)'])
+handle_linked_list.extract_singly_linked_list(root)
+print('-----')
+root = handle_linked_list.insert_into_single_linked_list(root, "newvalue", 0)
+handle_linked_list.extract_singly_linked_list(root)
+print('-----')
+handle_linked_list.insert_into_single_linked_list(root,"oldest_value", 1)
+handle_linked_list.extract_singly_linked_list(root)
+print('-----')
+root = handle_linked_list.pop_first(root)
+handle_linked_list.extract_singly_linked_list(root)
+"""
+#root = handle_linked_list.create_doubly_linked_list([1,'amine','ezra',15,15241,'JS SUCKS(probably bc i suck at it :)'])
+#handle_linked_list.extract_doubly_linked_list(root,1)
+#print(root.length-1)
