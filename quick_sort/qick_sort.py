0f06291ce9891ff460047671ab444221b4b86d8f
Amine Meftah <amine.gamer222@gmail.com>
2023-07-09 03:25:15 +0100

    quick sort was  a bit tricky to get !

--- quick_sort/qick_sort.py
+++ quick_sort/qick_sort.py
@@ -1,7 +1,4 @@
-
 def pivote_ (list, start , end):
-    
-    
     pivote = list[end]
     
     j = start
@@ -10,7 +7,6 @@ def pivote_ (list, start , end):
     while j<=end:
         
         if list[j] < pivote :
-            print(f"i>{i}, j>{j}, pivote:{pivote}")
             i+=1
             tmp = list[j]
             list[j] = list[i]
@@ -22,14 +18,16 @@ def pivote_ (list, start , end):
     tmp = list[i]
     list[i] = pivote
     list[end] = tmp
-
-    
     return i
+
 def quick_sort(list , start , end):
     if start>=end: return 0
     pivote = pivote_(list, start , end)
     quick_sort(list, start , pivote-1)
     quick_sort(list, pivote+1 , end)
+
+
+
 list = [1,0,2,3,7,5,10,9,8,6,4,12,40,2,5,53,3,74,346,632,1,0,-1]
 quick_sort(list, 0 , len(list)-1)
 print(list)
